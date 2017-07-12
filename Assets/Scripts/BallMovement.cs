5d386b17e98866d665ea5dddda44974476c0a9a2
Juan Manuel Rodriguez Defago <juanma.rdefago@gmail.com>
2017-07-12 04:00:04 -0300

    Primera version

---
+++ Assets/Scripts/BallMovement.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BallMovement : MonoBehaviour {
+
+    private Rigidbody2D rBody;
+    public int speed;
+    public float xVelocity;
+    public float yVelocity;
+    public Vector3 bounds;
+
+	// Use this for initialization
+	void Start () {
+        bounds = GetComponent<Collider2D>().bounds.extents * 0.5f;
+        rBody = GetComponent<Rigidbody2D>();
+        xVelocity = speed;
+        yVelocity = speed;
+        rBody.velocity = new Vector2(xVelocity, yVelocity);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        rBody.velocity = new Vector2(xVelocity, yVelocity);
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        Vector2 other = collision.contacts[0].point;
+        Debug.Log(other);
+        Debug.Log(transform.position);
+        if(other.x > transform.position.x + bounds.x || other.x < transform.position.x - bounds.x)
+        {
+            xVelocity = -xVelocity;
+        }
+        else if (other.y > transform.position.y + bounds.y || other.y < transform.position.y - bounds.y)
+        {
+            yVelocity = -yVelocity;
+        }
+    }
+
+}
---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour {
+
+    private Rigidbody2D rBody;
+    public int speed;
+
+	// Use this for initialization
+	void Start () {
+        rBody = GetComponent<Rigidbody2D>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        // Consigo el input horizontal (LeftArrow - RightArrow) con smoothing
+        float hInput = Input.GetAxis("Horizontal");
+        // Le cambio la velocidad al RigidBody2D por el Vector2 que tenga en X la velocidad * el input horizontal, y en la Y, 0.
+        rBody.velocity = new Vector2(hInput * speed, 0);        
+	}
+
+}
